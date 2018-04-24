44e910ea26969684153b2b69bcb36fa864460537
Arman.K <arman.koldaguc@student.hu.nl>
2018-04-24 11:41:34 +0200

    firstapp commit

--- src/main/java/nl/hu/v1wac/firstapp/servlets/DynamicServlet.java
+++ src/main/java/nl/hu/v1wac/firstapp/servlets/DynamicServlet.java
@@ -15,16 +15,16 @@ public class DynamicServlet extends HttpServlet{
 			throws ServletException, IOException {
 		int cijfer1 = Integer.parseInt(req.getParameter("cijfer1"));
 		int cijfer2 = Integer.parseInt(req.getParameter("cijfer2"));
-		String wiskundesymbool = req.getParameter("wiskundigesymbool");
+		String wiskundesymbool = req.getParameter("symbolen");
 		int eindAntwoord = 0;
-		
-		if(wiskundesymbool.equals("+")) {
+
+		if(wiskundesymbool.equals("plus")) {
 			eindAntwoord = cijfer1 + cijfer2;
-		}else if(wiskundesymbool.equals("-")) {
+		}else if(wiskundesymbool.equals("min")) {
 			eindAntwoord = cijfer1 - cijfer2;
-		}else if(wiskundesymbool.equals("*")) {
+		}else if(wiskundesymbool.equals("keer")) {
 			eindAntwoord = cijfer1 * cijfer2;
-		}else if(wiskundesymbool.equals("/")) {
+		}else if(wiskundesymbool.equals("delen")) {
 			eindAntwoord = cijfer1 / cijfer2;
 		}
 		
